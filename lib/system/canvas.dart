0d9ae392ecf9647683cfd5ae83ae1aee773ee057
playif <playif@gmail.com>
2014-07-12 02:15:29 +0800

    0.0.7 canvas

--- lib/system/canvas.dart
+++ lib/system/canvas.dart
@@ -1,6 +1,141 @@
 part of Phaser;
 
 class Canvas {
-  Canvas() {
+  static CanvasElement create([num width=256, num height=256, int id, bool noCocoon=false]) {
+    CanvasElement canvas = new CanvasElement();
+    if (id != null) {
+      canvas.dataset['id'] = id;
+    }
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.display = "block";
+    return canvas;
   }
+
+  static Point getOffset(CanvasElement element, [Point point]) {
+    if (point == null) {
+      point = new Point();
+    }
+    //point = point || new Phaser.Point();
+
+    var box = element.getBoundingClientRect();
+    int clientTop = element.clientTop;/// || document.body.clientTop || 0;
+    int clientLeft = element.clientLeft;// || document.body.clientLeft || 0;
+
+    //  Without this check Chrome is now throwing console warnings about strict vs. quirks :(
+
+    var scrollTop = 0;
+    var scrollLeft = 0;
+
+    if (document.compatMode == 'CSS1Compat') {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop || element.scrollTop || 0;
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft || element.scrollLeft || 0;
+    }
+    else {
+      scrollTop = window.pageYOffset || document.body.scrollTop || element.scrollTop || 0;
+      scrollLeft = window.pageXOffset || document.body.scrollLeft || element.scrollLeft || 0;
+    }
+
+    point.x = box.left + scrollLeft - clientLeft;
+    point.y = box.top + scrollTop - clientTop;
+
+    return point;
+
+  }
+
+  static num getAspectRatio (CanvasElement canvas) {
+    return canvas.width / canvas.height;
+  }
+
+  static CanvasElement setBackgroundColor (CanvasElement canvas, [String color='rgb(0,0,0)']) {
+    canvas.style.backgroundColor = color;
+    return canvas;
+  }
+
+//  CanvasElement setTouchAction (CanvasElement canvas, [String value='none']) {
+//    //value = value || 'none';
+//    canvas.style.msTouchAction = value;
+//    canvas.style['ms-touch-action'] = value;
+//    canvas.style['touch-action'] = value;
+//    return canvas;
+//  }
+
+  static CanvasElement setUserSelect (CanvasElement canvas, [String value='none']) {
+    canvas.style.userSelect = value;
+//    canvas.style['-webkit-user-select'] = value;
+//    canvas.style['-khtml-user-select'] = value;
+//    canvas.style['-moz-user-select'] = value;
+//    canvas.style['-ms-user-select'] = value;
+//    canvas.style['user-select'] = value;
+    canvas.style.tapHighlightColor = 'rgba(0, 0, 0, 0)';
+    return canvas;
+  }
+
+  static CanvasElement addToDOM (CanvasElement canvas, [parent, bool overflowHidden=true]) {
+
+    HtmlElement target;
+
+    if (parent != null)
+    {
+      if (parent is String)
+      {
+        // hopefully an element ID
+        target = document.getElementById(parent);
+      }
+      else if (parent is HtmlElement)
+      {
+        // quick test for a HTMLelement
+        target = parent;
+      }
+    }
+
+    // Fallback, covers an invalid ID and a non HTMLelement object
+    if (target == null)
+    {
+      target = document.body;
+    }
+
+    if (overflowHidden)
+    {
+      target.style.overflow = 'hidden';
+    }
+
+    target.append(canvas);
+
+    return canvas;
+
+  }
+
+  static CanvasRenderingContext2D setTransform (CanvasRenderingContext2D context, num translateX,num translateY,num scaleX,num scaleY,num skewX,num skewY) {
+    context.setTransform(scaleX, skewX, skewY, scaleY, translateX, translateY);
+    return context;
+  }
+
+  static CanvasRenderingContext2D setSmoothingEnabled (CanvasRenderingContext2D context, [bool value=false]) {
+    context.imageSmoothingEnabled=value;
+//    context['imageSmoothingEnabled'] = value;
+//    context['mozImageSmoothingEnabled'] = value;
+//    context['oImageSmoothingEnabled'] = value;
+//    context['webkitImageSmoothingEnabled'] = value;
+//    context['msImageSmoothingEnabled'] = value;
+    return context;
+  }
+
+  static CanvasElement setImageRenderingCrisp (CanvasElement canvas) {
+    canvas.style.imageRendering='optimize-contrast';
+//    canvas.style['image-rendering'] = 'optimizeSpeed';
+//    canvas.style['image-rendering'] = 'crisp-edges';
+//    canvas.style['image-rendering'] = '-moz-crisp-edges';
+//    canvas.style['image-rendering'] = '-webkit-optimize-contrast';
+//    canvas.style['image-rendering'] = 'optimize-contrast';
+//    canvas.style.msInterpolationMode = 'nearest-neighbor';
+    return canvas;
+  }
+
+  static CanvasElement setImageRenderingBicubic (CanvasElement canvas) {
+    canvas.style.imageRendering = 'auto';
+    //canvas.style.msInterpolationMode = 'bicubic';
+    return canvas;
+  }
+
 }
