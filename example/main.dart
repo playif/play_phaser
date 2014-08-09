93ced6a09dc6eb1c915b87cf4560cdef1108c8ab
playif <playif@gmail.com>
2014-08-09 23:10:38 +0800

    0.8.6 ie worked fine

--- example/main.dart
+++ example/main.dart
@@ -98,28 +98,28 @@ main() {
 //  dom.window.console.log("preload");
 
 
-  var w = dom.window.innerWidth * dom.window.devicePixelRatio,
-  h = dom.window.innerHeight * dom.window.devicePixelRatio,
-  width = (h > w) ? h : w,
-  height = (h > w) ? w : h;
+//  var w = dom.window.innerWidth * dom.window.devicePixelRatio,
+//  h = dom.window.innerHeight * dom.window.devicePixelRatio,
+//  width = (h > w) ? h : w,
+//  height = (h > w) ? w : h;
+//
+//  // Hack to avoid iPad Retina and large Android devices. Tell it to scale up.
+//  if (dom.window.innerWidth >= 1024 && dom.window.devicePixelRatio >= 2)
+//  {
+//    width = Math.round(width / 2);
+//    height = Math.round(height / 2);
+//  }
+//  // reduce screen size by one 3rd on devices like Nexus 5
+//  if (dom.window.devicePixelRatio == 3)
+//  {
+//    width = Math.round(width / 3) * 2;
+//    height = Math.round(height / 3) * 2;
+//  }
 
-  // Hack to avoid iPad Retina and large Android devices. Tell it to scale up.
-  if (dom.window.innerWidth >= 1024 && dom.window.devicePixelRatio >= 2)
-  {
-    width = Math.round(width / 2);
-    height = Math.round(height / 2);
-  }
-  // reduce screen size by one 3rd on devices like Nexus 5
-  if (dom.window.devicePixelRatio == 3)
-  {
-    width = Math.round(width / 3) * 2;
-    height = Math.round(height / 3) * 2;
-  }
-
-  var game = new Game(width, height, CANVAS, '');
+  //var game = new Game(width, height, CANVAS, '');
 
 
-  //Game game = new Game(800, 600, CANVAS, '');
+  Game game = new Game(800, 600, CANVAS, '');
 
   dom.SelectElement select = dom.document.getElementById("examples") as dom.SelectElement;
   for (String key in examples.keys) {
